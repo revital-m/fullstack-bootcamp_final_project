539979bc6d587d6b0b944b8b5b90e9b39d5d2c9c
revital-m <revital13m@gmail.com>
2022-02-02 16:37:24 +0200

    added Jobs modle & routes with auth.

---
+++ src/models/jobs.js
@@ -0,0 +1,79 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const jobsSchema = new mongoose.Schema({
+  jobDescription: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  companyName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  contacts: {
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is invalid");
+        }
+      },
+    },
+    fullName: {
+      type: String,
+      trim: true,
+    },
+    phone: {
+      type: String,
+      validate(value) {
+        if (!validator.isMobilePhone(value, "he-IL")) {
+          throw new Error("Phone is invalid");
+        }
+      },
+    },
+  },
+  moreInfo: [
+    {
+      date: {
+        type: Date,
+        default: new Date(),
+      },
+      info: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+  timeline: {
+    sendCV: {
+      type: Boolean,
+      default: true,
+    },
+    gotCallback: {
+      type: Boolean,
+      default: false,
+    },
+    interview: {
+      type: Boolean,
+      default: false,
+    },
+    negotiation: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: "User",
+  },
+});
+
+const Jobs = mongoose.model("Jobs", jobsSchema);
+
+module.exports = Jobs;
--- src/models/user.js
+++ src/models/user.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
 const jwt = require("jsonwebtoken");
+const Jobs = require("./jobs");
 
 const userSchema = new mongoose.Schema({
   password: {
@@ -36,14 +37,6 @@ const userSchema = new mongoose.Schema({
     {
       token: {
         type: String,
-        required: true,
-      },
-    },
-  ],
-  jobs: [
-    {
-      jobID: {
-        type: mongoose.Schema.Types.ObjectId,
         required: true,
       },
     },
@@ -90,6 +83,12 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+userSchema.virtual("jobs", {
+  ref: "Jobs",
+  localField: "_id",
+  foreignField: "owner",
+});
+
 //* Remove confidential information from the response that the user is going to get back.
 userSchema.methods.toJSON = function () {
   const user = this;
@@ -105,9 +104,13 @@ userSchema.methods.toJSON = function () {
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
 
-  const token = jwt.sign({ _id: user._id.toString() }, `${process.env.ACCESS_TOKEN_SECRET}`, {
-    expiresIn: "24 hours",
-  });
+  const token = jwt.sign(
+    { _id: user._id.toString() },
+    `${process.env.ACCESS_TOKEN_SECRET}`,
+    {
+      expiresIn: "24 hours",
+    }
+  );
 
   user.tokens = user.tokens.concat({ token });
   await user.save();
@@ -138,12 +141,21 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
+//* Delete the user's jobs when the user is removed.
+userSchema.pre("remove", async function (next) {
+  const user = this;
+  await Jobs.deleteMany({ owner: user._id });
+
+  next();
+});
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = User;
 
 /** 
- * Check if a password is strong or not. Allows for custom requirements or scoring rules. If returnScore is true, then the function returns an integer score for the password rather than a boolean.
-Default options:
-{ minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 }
+ //! Strong Password Conditions:
+  Check if a password is strong or not. Allows for custom requirements or scoring rules. If returnScore is true, then the function returns an integer score for the password rather than a boolean.
+  Default options:
+  { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 }
 */
